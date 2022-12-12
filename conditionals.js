36f0e16d10846020703bb67659ecc4d05b97730d
saaiid <ibrosakid@gmail.com>
2022-12-12 18:29:57 -0500

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -54,3 +54,100 @@ const age = 26;
 if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
 }
+
+//confusing loop
+var x = 10;
+while (x <= 25) {
+  console.log('Printing out x = ' + x);
+  x = x + 2;
+}
+
+// fizzbuzz while  loop
+var x = 1;
+
+// while loop with a stop condition
+while (x <= 20) {
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log("JuliaJames");
+    }else if (x % 5 === 0) {
+        console.log("James");
+    }else if (x % 3 === 0) {
+        console.log("Julia");
+    }
+    else {
+        console.log(x);
+    }
+    // increment x at the end of each loop
+    x = x + 1 ; 
+}
+
+var num = 99;
+
+while (num >=1) {
+    // Last iteration. Note occurances of bottle, bottle, bottleS
+    if (num === 1) {
+        console.log(num + " bottle of juice on the wall! "
+                    + num + " bottle of juice! Take one down, pass it around... "
+                    + (num-1) + " bottles of juice on the wall!");
+    }
+    // Second-last iteration. Note occurances of bottleS, bottleS, bottle
+    else if (num === 2){
+        console.log(num + " bottles of juice on the wall! "
+                    + num + " bottles of juice! Take one down, pass it around... "
+                    + (num-1) + " bottle of juice on the wall!");
+    }
+    // All other iterations. Note occurances of bottleS, bottleS, bottleS
+    else {
+        console.log(num + " bottles of juice on the wall! "
+                    + num + " bottles of juice! Take one down, pass it around... "
+                    + (num-1) + " bottles of juice on the wall!");
+    }
+    num = num - 1;
+}
+
+// Let us take an iteration variable, that represent the remaining Time in seconds
+n = 60
+// While loop with a stop condition
+while (n >= 0) {
+  if (n === 50) {
+      console.log("Orbiter transfers from ground to internal power");
+  }
+  else if (n === 31) {
+      console.log("Ground launch sequencer is go for auto sequence start");
+  }
+  else if (n === 16) {
+      console.log("Activate launch pad sound suppression system");
+  }
+  else if (n === 10) {
+      console.log("Activate main engine hydrogen burnoff system");
+  }
+  else if (n === 6) {
+      console.log("Main engine start");
+  }
+  else if (n === 0) {
+      console.log("Solid rocket booster ignition and liftoff!");
+  }
+  else{
+      console.log("T-"+n+" seconds");
+  }
+
+  //Never forget to decrement/increment the iteration variable in a while loop
+  // Otherwise, you loop will run infinite iterations
+  n = n-1;
+}
+
+//for loop that find your sit number
+
+var row = 0;  // initial value of the row
+var seat = 0; // initial value of the seat within a row
+
+// One loop inside another is called Nested loop.
+// Outer `for` loop, to iterate over the rows
+for (row = 0; row <= 25; row++){
+
+    // Inner `for` loop, to iterate over the seats within a row
+    // In this loop, the value of `row` variable would change only after 100 iterations
+    for(seat = 0; seat <= 99; seat++){
+        console.log(row+"-"+seat);
+    }
+}
