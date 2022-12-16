735797bdfc934b56e5ab5ac0dddee2563e852346
saaiid <ibrosakid@gmail.com>
2022-12-15 19:39:26 -0500

    Add this basic function example

---
+++ hello.js
@@ -0,0 +1,22 @@
+//basic function
+/* const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello(); */
+
+//function example2
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
+
+//example 3
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
