114a8208206665711bfdb121945dbbbe0ba230a7
saaiid <ibrosakid@gmail.com>
2022-12-17 16:23:39 -0500

    changing an array using for loop

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -158,3 +158,21 @@ function movies(messageFunction, name) {
 
 // Call the movies function, pass in the favoriteMovie function and name of movie
 movies(favoriteMovie, "Finding Nemo");
+
+
+//changing an array using for loop
+for (var index = 0; index<prices.length; index++){
+  //change the value of first element
+  if(index===0){
+      prices[index]=11;
+  }
+  //change the value of third element
+  else if(index===2){
+      prices[index]=33;
+  }
+  //change the value of seventh element
+  else if(index===6){
+      prices[index]=77;
+  }
+}
+console.log(prices);
