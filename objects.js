476ff6a3e3ed3a4682526ed9468c163af75d2457
saaiid <ibrosakid@gmail.com>
2022-12-20 21:07:48 -0500

    object dot notation

---
+++ objects.js
@@ -0,0 +1,9 @@
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni'],
+  crust: 'deep dish',
+  serves: 2
+}
+
+console.log(pizza.toppings);
+console.log(pizza.crust);
+console.log(pizza.serves);
